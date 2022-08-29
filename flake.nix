03fd1e76b03efe2aa39c0a8c5552482f2854b2c5
Martin Toman <toman.martin@live.com>
2022-08-29 12:44:36 -0700

    Put node module in $out/lib/node_modules/node-Tor/

--- flake.nix
+++ flake.nix
@@ -10,7 +10,44 @@
 
     in {
       overlays.default = final: prev: {
-        node-Tor = final.callPackage ./default.nix { };
+        node-Tor = with final; stdenv.mkDerivation rec {
+          pname = "node-Tor";
+          version = "0.0.0";
+
+          src = ./.;
+          buildInputs = [
+            nodePackages.browserify
+            nodePackages.terser
+          ];
+
+          buildPhase = ''
+            cd lib
+            browserify node-tor.js > ../html/browser.js
+            cd ../html
+            terser browser.js -c -m -o minified.js
+            cd ..
+          '';
+          installPhase = ''
+            # lib/node_modules
+            mkdir -p $out/lib/node_modules/node-Tor
+            cp -r lib/* $out/lib/node_modules/node-Tor
+            mv $out/lib/node_modules/node-Tor/node-tor.js \
+               $out/lib/node_modules/node-Tor/node-Tor.js
+            cat > $out/lib/node_modules/node-Tor/package.json << EOF
+            {
+              "name": "${pname}",
+              "version": "${version}",
+              "license": "MIT",
+              "main": "node-Tor.js"
+            }
+            EOF
+
+            # lib/browser
+            mkdir -p $out/lib/browser
+            cp html/browser.js $out/lib/browser/node-Tor.js
+            cp html/minified.js $out/lib/browser/node-Tor.min.js
+          '';
+        };
       };
 
       packages = forAllSystems (system:
